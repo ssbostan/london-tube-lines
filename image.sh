0d145b510f0a5dfb0d5d1c69eda9ae122f3e4f31
Saeid Bostandoust <ssbostan@yahoo.com>
2022-10-24 00:47:35 +0100

    update build script

--- image.sh
+++ image.sh
@@ -1,22 +1,12 @@
 #!/usr/bin/bash
 
-docker build -f backend/Dockerfile \
-  -t london-tube-lines-backend:latest ./backend
+docker build -t ssbostan/london-tube-lines-backend:latest ./backend
 
-docker build -f frontend/Dockerfile \
-  -t london-tube-lines-frontend:latest ./frontend
+docker build -t ssbostan/london-tube-lines-frontend:latest ./frontend
 
-docker tag \
-  london-tube-lines-backend:latest \
-  ssbostan/london-tube-lines-backend:latest
+read -p "Do you what to push the images to DockerHub registry? (yes/no) " PUSH_TO_HUB
 
-docker tag \
-  london-tube-lines-frontend:latest \
-  ssbostan/london-tube-lines-frontend:latest
-
-read -p "Do you what to push the images to DockerHub registry? (yes/no) " PUSH_TO_DOCKERHUB
-
-if [[ $PUSH_TO_DOCKERHUB == "yes" ]]; then
+if [[ $PUSH_TO_HUB == "yes" ]]; then
     docker push ssbostan/london-tube-lines-backend:latest
     docker push ssbostan/london-tube-lines-frontend:latest
 fi
